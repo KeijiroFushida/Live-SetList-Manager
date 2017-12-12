22fcf5f50d16862e267763478472fa110fcaf847
Keijirou Fushida <Keijirou Fushida>
2017-12-12 22:17:41 +0900

    migration

---
+++ database/migrations/2017_12_12_123148_create_music.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMusic extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events',function(Blueprint $table)) {
+            $table->increments('id');
+            $table->unsigned_integer('artist_id');
+            $table->foreign('artist_id')->reference('id')->on('artists');
+            $table->integer('live_id');
+            $table->string('music_name');
+            $table->integer('set_list');
+            $table->timestamp();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
---
+++ database/migrations/2017_12_12_123930_create_artist_event_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArtistEventTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artist_event', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsigned_integer('artist_id');
+            $table->foreign('artist_id')->reference('id')->on('artists');
+            $table->unsigned_integer('event_id');
+            $table->foreign('event_id')->reference('id')->on('events');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('artist_event');
+    }
+}
