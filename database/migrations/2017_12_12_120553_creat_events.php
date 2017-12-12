8842e4252921071e3b7b7c87a3590ec487b600e7
Keijirou Fushida <Keijirou Fushida>
2017-12-12 22:23:24 +0900

    migration

--- database/migrations/2017_12_12_120553_creat_events.php
+++ database/migrations/2017_12_12_120553_creat_events.php
@@ -13,7 +13,7 @@ class CreatEvents extends Migration
      */
     public function up()
     {
-        Schema::create('events',function(Blueprint $table)) {
+        Schema::create('events',function(Blueprint $table) {
         	$table->increments('id');
         	$table->string('event_name');
         	$table->string('event_place');
