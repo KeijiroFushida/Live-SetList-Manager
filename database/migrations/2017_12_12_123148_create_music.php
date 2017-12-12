dd1c4997ff0314c4ccbe775d191ea0f72c1c41b6
Keijirou Fushida <Keijirou Fushida>
2017-12-12 22:23:54 +0900

    migration

--- database/migrations/2017_12_12_123148_create_music.php
+++ database/migrations/2017_12_12_123148_create_music.php
@@ -13,7 +13,7 @@ class CreateMusic extends Migration
      */
     public function up()
     {
-        Schema::create('events',function(Blueprint $table)) {
+        Schema::create('events',function(Blueprint $table) {
             $table->increments('id');
             $table->unsigned_integer('artist_id');
             $table->foreign('artist_id')->reference('id')->on('artists');
