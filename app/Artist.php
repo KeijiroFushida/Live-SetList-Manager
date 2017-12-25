55886745a2eec8aebe180787f5fe4e9c211f2d26
fox-yudai-saito <yudai.saito@fox-hound.jp>
2017-12-25 16:26:30 +0900

    クラス名も修正

--- app/Artist.php
+++ app/Artist.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Artists extends Model
+class Artist extends Model
 {
 
 	protected $fillable = [
--- app/Music.php
+++ app/Music.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Musics extends Model
+class Music extends Model
 {
 	protected $fillable = [
 		'event_id',
